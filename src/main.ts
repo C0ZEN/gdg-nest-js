38652cebb299bf032a7053bda9d95214c2f59ae5
Aurélien Loyer <aur.loy@gmail.com>
2019-11-02 19:02:58 +0100

    feat: step0 done ✅

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(process.env.PORT || 3000);
+
+  console.log(`App ready to serve on port ${process.env.PORT || 3000}`);
+}
+
+bootstrap();
