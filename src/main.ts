6388210ef68b7aeef3867bf735f5eb50c9f6a962
Aurélien Loyer <aur.loy@gmail.com>
2019-11-03 23:17:33 +0100

    feat: add swagger 📈

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 
 import { AppModule } from './app.module';
@@ -12,6 +13,16 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'static'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  const options = new DocumentBuilder()
+    .setTitle('Codelab Nest - Products API')
+    .setDescription('An API to retrieve products informations')
+    .setVersion('1.0')
+    .addBearerAuth('Authorization', 'header')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 
