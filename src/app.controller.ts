725c1d3c3384d178acb956dbb0e99f51ea1d86c5
Aurélien Loyer <aur.loy@gmail.com>
2020-12-08 22:41:42 +0100

    feat: update nestjs to version 7 🎉

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,15 @@
-import { Get, Controller, Render, Param, Res } from '@nestjs/common';
-import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { Get, Controller, Param, Res, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-    @Get()
-    @Render('index')
-    root() {
-        return { message: 'Hello world ! 🦄' };
-    }
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Hello world ! 🦄' };
+  }
 
-    @Get('static/img/:imageName')
-    image(@Param('imageName') imageName, @Res() res) {
-        return res.sendFile(`img/${imageName}`, { root: 'static' });
-    }
+  @Get('static/img/:imageName')
+  image(@Param('imageName') imageName, @Res() res) {
+    return res.sendFile(`img/${imageName}`, { root: 'static' });
+  }
 }
